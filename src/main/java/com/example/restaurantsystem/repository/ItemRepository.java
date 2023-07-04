44bfb8f9a364b12088281f319dc1a3d0e2968425
alsu-ismayilzada <ismayilzadealsu@gmail.com>
2023-07-04 11:50:50 +0400

    Repository

---
+++ src/main/java/com/example/restaurantsystem/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.restaurantsystem.repository;
+
+import com.example.restaurantsystem.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item,Integer> {
+}
