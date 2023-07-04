8e0f2d60529544f4e9ff83aaeba5e223ab2673e6
alsu-ismayilzada <ismayilzadealsu@gmail.com>
2023-07-04 13:05:59 +0400

    Service

--- src/main/java/com/example/restaurantsystem/service/OrderService.java
+++ src/main/java/com/example/restaurantsystem/service/OrderService.java
@@ -1,14 +1,13 @@
 package com.example.restaurantsystem.service;
 
 import com.example.restaurantsystem.dto.OrderDto;
-import com.example.restaurantsystem.entity.Order;
 
 import java.util.List;
 
 public interface OrderService {
 
     void addOrder(OrderDto order);
-    void rejectById(Integer id);
+    void deleteById(Integer id);
     OrderDto getById(Integer id);
     List<OrderDto> getAll();
 }
--- src/main/java/com/example/restaurantsystem/service/ReservationService.java
+++ src/main/java/com/example/restaurantsystem/service/ReservationService.java
@@ -1,14 +1,13 @@
 package com.example.restaurantsystem.service;
 
 import com.example.restaurantsystem.dto.ReservationDto;
-import com.example.restaurantsystem.entity.Reservation;
 
 import java.util.List;
 
 public interface ReservationService {
 
     void addReservation(ReservationDto reservation);
-    void rejectById(Integer id);
+    void deleteById(Integer id);
     ReservationDto getById(Integer id);
     List<ReservationDto> getAll();
 }
